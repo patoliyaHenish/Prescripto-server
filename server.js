a099c52555dbdc298700dd2b182fd151012be736
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-12-03 14:24:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connnectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
+
+// app config
+const app = express();
+const port = process.env.PORT || 3000;  
+connectDB();
+connnectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.use('/api/admin', adminRouter) // localhost:4000/api/admin/add-doctor
+app.use('/api/doctor', doctorRouter) // localhost:4000/api/doctor/list
+app.use('/api/user', userRouter)
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(port, () => console.log(`Server is running on port http://localhost:${port}/`));
